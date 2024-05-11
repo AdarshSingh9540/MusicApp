86aa40581b1ba251398d39af5759b9f5205b9630
Adarsh <adarsh9540984202@gmail.com>
2024-05-11 16:40:29 +0530

    navbar update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,8 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
+  darkMode: 'class',
+    theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
