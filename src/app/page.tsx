b1d6916028eee22057f7340b65739d5e41b888a5
Adarsh <adarsh9540984202@gmail.com>
2024-05-11 18:22:47 +0530

    Hero section is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      {/* hi there */}
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      {/* <h1 className="text-2xl text-center text-white ">Chain aiur njn</h1> */}
+       <HeroSection/>
+    </main>
     
   );
 }
